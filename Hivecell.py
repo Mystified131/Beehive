372309784d74f9e3da7aca31f4b1bd9541b26f6a
mystified131 <mystifiedthomas@gmail.com>
2018-11-10 06:12:39 -0600

    Streamlining workability

--- Hivecell.py
+++ Hivecell.py
@@ -1,4 +1,4 @@
-textchunk = "Surrounded to me occasional."
+textchunk = "Surrounded to me occasional pianoforte alteration unaffected impossible ye. For saw half than cold. Pretty merits waited six talked pulled you. Conduct replied off led whether any shortly why arrived adapted. Numerous ladyship so raillery humoured goodness received an. So narrow formal length my highly longer afford oh. Tall neat he make or at dull ye."
 
 wordlist = []
 wordlst = []
@@ -19,9 +19,11 @@ if len(wordlst) < 6:
         texttemp += wordlst[elem] + " "
     texttemp = texttemp.rstrip()
     phraselist.append(texttemp)
+    #return phraselist
 
 if len(wordlst) >= 6:
-    for num1 in range(len(wordlst)):
+    for numb in range(int(len(wordlst)/3)):
+        num1 = numb * 3
         texttemp = ""
         texttemp += (wordlst[num1 - 6] + " " + wordlst[num1 - 5] + " " + wordlst[num1 - 4] + " " + wordlst[num1 - 3] + " " + wordlst[num1 - 2] + " " + wordlst[num1 - 1])
         phraselist.append(texttemp)
